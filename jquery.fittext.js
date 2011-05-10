7ce7b61261c49fc74d00a520467a22109e8259fd
Manuel Strehl <com.github@manuel-strehl.de>
2011-05-10 11:43:21 -0700

    Use good ol' Math.min

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -14,15 +14,13 @@
 	$.fn.fitText = function( kompressor ) {
 	
 			return this.each(function(){
-				var $this = $(this);                                      // store the object
-				var fontResize = origFontSize = $this.css('font-size');   // init the font sizes
-				var compressor = kompressor || 1;                         // set the compressor
+				var $this = $(this);                         // store the object
+				var origFontSize = $this.css('font-size');   // init the font sizes
+				var compressor = kompressor || 1;            // set the compressor
 
         // Resizer() resizes items based on the object width divided by the compressor * 10
 				var resizer = function ( obj ) {
-					fontResize =  obj.width() / (compressor*10);
-					fontResize = (fontResize >= origFontSize)?  origFontSize : fontResize; 
-					obj.css('font-size',fontResize);
+					obj.css('font-size', Math.min(obj.width() / (compressor*10), origFontSize));
 				}
 
 				// Call once to set.
