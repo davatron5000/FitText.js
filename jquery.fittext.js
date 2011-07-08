3adbe5373d1828976afd21c2e93da705c6f3e5ba
davatron5000 <rupato@gmail.com>
2011-07-08 18:05:17 -0500

    Added experimental min-font-size

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -11,16 +11,17 @@
 
 (function( $ ){
 	
-	$.fn.fitText = function( kompressor ) {
+	$.fn.fitText = function( kompressor, minSize ) {
 	
 			return this.each(function(){
 				var $this = $(this);                                     // store the object
-				var origFontSize = parseFloat($this.css('font-size'));   // init the font sizes
+				var maxFontSize = parseFloat($this.css('font-size'));    // simulate max-font-size based off of CSS specified font-size
+				var minFontSize = parseFloat(minSize) || 0;              // Optional min-font-size simulator. Use pixels.
 				var compressor = kompressor || 1;                        // set the compressor
 
         // Resizer() resizes items based on the object width divided by the compressor * 10
 				var resizer = function () {
-					$this.css('font-size', Math.min($this.width() / (compressor*10), origFontSize));
+					$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), maxFontSize), minFontSize));
 				};
 
 				// Call once to set.
