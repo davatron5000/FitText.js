ae993433b465bc7420b95643821049a9b35208e1
Dave Rupert <rupato@gmail.com>
2012-04-27 17:58:30 -0500

    Soft tabs

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -11,34 +11,34 @@
 
 (function( $ ){
 	
-	$.fn.fitText = function( kompressor, options ) {
+  $.fn.fitText = function( kompressor, options ) {
 	    
-	    var settings = {
-        'minFontSize' : Number.NEGATIVE_INFINITY,
-        'maxFontSize' : Number.POSITIVE_INFINITY
-      };
+    var settings = {
+      'minFontSize' : Number.NEGATIVE_INFINITY,
+      'maxFontSize' : Number.POSITIVE_INFINITY
+    };
 	
-			return this.each(function(){
-				var $this = $(this);              // store the object
-				var compressor = kompressor || 1; // set the compressor
+    return this.each(function(){
+      var $this = $(this);              // store the object
+		  var compressor = kompressor || 1; // set the compressor
         
-        if ( options ) { 
-          $.extend( settings, options );
-        }
+      if ( options ) { 
+        $.extend( settings, options );
+      }
         
-        // Resizer() resizes items based on the object width divided by the compressor * 10
-				var resizer = function () {
-					$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-				};
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+		  var resizer = function () {
+		    $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+		  };
 
-				// Call once to set.
-				resizer();
+      // Call once to set.
+      resizer();
 				
-				// Call on resize. Opera debounces their resize by default. 
-      	$(window).resize(resizer);
+      // Call on resize. Opera debounces their resize by default. 
+      $(window).resize(resizer);
       	
-			});
+    });
 
-	};
+  };
 
 })( jQuery );
