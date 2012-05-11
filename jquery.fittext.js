64ad955e6420b0a9186861cdac4b241bce73efc7
Phil Ricketts <phil@replete.nu>
2012-05-11 17:27:19 +0200

    -Added new `off` function to disable fitText. -Added `event` option, which allows you to define what event fitText fires on (useful if using custom debounced resize events, for example)

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -1,44 +1,56 @@
-/*global jQuery */
-/*!	
-* FitText.js 1.0
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license 
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/
-
+//FitText.js
 (function( $ ){
 
-  $.fn.fitText = function( kompressor, options ) {
+	$.fn.fitText = function( kompressor, options ) {
 
-    var settings = {
-      'minFontSize' : Number.NEGATIVE_INFINITY,
-      'maxFontSize' : Number.POSITIVE_INFINITY
-    };
+		var settings = {
+			'minFontSize' : Number.NEGATIVE_INFINITY,
+			'maxFontSize' : Number.POSITIVE_INFINITY,
+			'event' : "resize.fitText"  //You can use any event e.g. bespoke debounced resize like 'smartresize'
+			},
+			$obj = (this);
 
-    return this.each(function(){
-      var $this = $(this);              // store the object
-		  var compressor = kompressor || 1; // set the compressor
-        
-      if ( options ) { 
-        $.extend( settings, options );
-      }
-        
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
+		this
+		.each(function(){
 
-      // Call once to set.
-      resizer();
+			var $this = $(this), // store the object
+				compressor = kompressor || 1; // set the compressor
 
-      // Call on resize. Opera debounces their resize by default.
-      $(window).bind("resize.fittext",resizer);
-      
-    });
+			if ( options ) { 
+				$.extend( settings, options );
+			}
 
-  };
+			// Resizer() resizes items based on the object width divided by the compressor * 10
+			var resizer = function () {
+				$this
+				.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+			};
 
+			// Call once to set.
+			resizer();
+
+			// Call on resize. Opera debounces their resize by default.
+			$(window)
+			.bind(settings.event,resizer);
+
+		});
+
+		return {
+
+			off: function() {
+
+				//Unbind FitText event
+				$(window)
+				.unbind(settings.event);
+
+				//Remove font-size inline style
+				$obj
+				.each(function(){
+					
+					$(this)
+					.css('font-size',''); 
+				});
+			}
+		}
+	}
 })( jQuery );
