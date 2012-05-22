e8158a687ac6f94489571ae4fca90c6434a4edb3
David DeSandro <david@desandro.com>
2012-05-22 07:18:11 -0400

    whitespace fixes

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -1,31 +1,31 @@
 /*global jQuery */
-/*!	
+/*!
 * FitText.js 1.0
 *
 * Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license 
+* Released under the WTFPL license
 * http://sam.zoy.org/wtfpl/
 *
 * Date: Thu May 05 14:23:00 2011 -0600
 */
 
 (function( $ ){
-	
+
   $.fn.fitText = function( kompressor, options ) {
-	    
+
     var settings = {
       'minFontSize' : Number.NEGATIVE_INFINITY,
       'maxFontSize' : Number.POSITIVE_INFINITY
     };
-	
+
     return this.each(function(){
       var $this = $(this);              // store the object
-		  var compressor = kompressor || 1; // set the compressor
-        
-      if ( options ) { 
+      var compressor = kompressor || 1; // set the compressor
+
+      if ( options ) {
         $.extend( settings, options );
       }
-        
+
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
         $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
@@ -33,10 +33,10 @@
 
       // Call once to set.
       resizer();
-				
-      // Call on resize. Opera debounces their resize by default. 
+
+      // Call on resize. Opera debounces their resize by default.
       $(window).resize(resizer);
-      	
+
     });
 
   };
