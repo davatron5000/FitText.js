5d350746a65dfa481b66aaa907c72640c6ca24c8
Gary Hepting <ghepting@gmail.com>
2012-06-14 16:38:11 -0700

    added rounding to avoid non integer pixel values

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -28,7 +28,7 @@
         
         // Resizer() resizes items based on the object width divided by the compressor * 10
 				var resizer = function () {
-					$this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+					$this.css('font-size', Math.round(Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize))));
 				};
 
 				// Call once to set.
