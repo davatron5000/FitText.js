4141ba84f5cd587395e881dde778d604c1f528a4
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2013-04-12 10:57:47 +0300

    Use requestAnimationFrame for improved perfs

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -9,7 +9,12 @@
 * Date: Thu May 05 14:23:00 2011 -0600
 */
 
-(function( $ ){
+(function( $, window ){
+
+  window.requestAnimationFrame = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame 
+    || window.mozRequestAnimationFrame
+    || function( callback ){ window.setTimeout(callback, 1000 / 60); };
 
   $.fn.fitText = function( kompressor, options ) {
 
@@ -19,25 +24,39 @@
           'minFontSize' : Number.NEGATIVE_INFINITY,
           'maxFontSize' : Number.POSITIVE_INFINITY
         }, options);
-
+        
     return this.each(function(){
 
       // Store the object
       var $this = $(this);
 
+      // Is requestAnimationFrame running?
+      var raf = false;
+          
+      var onResize = function() {
+        // make sure we don't stack raf calls uselessly
+        if( !raf ) {
+          window.requestAnimationFrame(resizer);
+        }
+        raf = true;
+      }
+
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
         $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        
+        // start capturing events again
+        raf = false;
       };
 
       // Call once to set.
       resizer();
 
       // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize orientationchange', resizer);
+      $(window).on('resize orientationchange', onResize);
 
     });
 
   };
 
-})( jQuery );
+})( jQuery, window );
