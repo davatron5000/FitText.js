810b7f698a8cc1d9b35eba7967cf73ea70a0cb48
Saint Clair <saintclair.com@gmail.com>
2013-09-12 17:28:16 -0300

    [Fixed] getting width via clientWidth

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -26,14 +26,14 @@
       var $this = $(this);
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
+      var resizer = function () { 
 		$this[0].style.fontSize = Math.max(
 			Math.min(
-				$this[0].width() / (compressor*10),
+				$this[0].clientWidth / (compressor*10),
 				parseFloat(settings.maxFontSize)
 			),
 			parseFloat(settings.minFontSize)
-		) +'px'; // Don't forget to add PX value or it breaks
+		).toFixed(2) +'px'; // Don't forget to add PX value or it breaks
       };
 
       // Call once to set.
