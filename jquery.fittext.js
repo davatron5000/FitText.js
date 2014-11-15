a1dba26df5273f1ef87e3664b5f003742a7bc3e0
iAmNathanJ <jacobs.123@gmail.com>
2014-11-14 23:50:24 -0500

    adding support for em, rem, vw, and special pw (parentWidth) units. Allows for different units inmin/maxFontSize settings... i.e. Can set a fluid length against a fixed pixel length.

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -12,7 +12,46 @@
 (function( $ ){
 
   $.fn.fitText = function( kompressor, options ) {
+    
+    var $body   = $('body'),
+        $parent = $(this).parent(),
+        variableFontSize;
 
+    // Get sizing units
+    var getUnits = function(fontSize){
+        return String(fontSize).replace(/[0-9.]/g, '').toLowerCase();
+    };
+    
+    var calculateUnits = {
+      
+      // Calculate em
+      em: function(size){
+        var parentFontSize = parseInt($parent.css('font-size'));
+        return parentFontSize * size;
+      },
+    
+      // Calculate rem (Set root size on <html>)
+      rem: function(size){
+        var rootFontSize = parseInt($body.css('font-size'));
+        return rootFontSize * size;
+      },
+
+      // Calculate vw (View based on $('body') width)
+      // This works acurately with compressor set to 0.1 and allows for fixed pixel minimum
+      vw: function(size){
+        var viewWidth = $body.width();
+        return Math.ceil((viewWidth / 100) * size);
+      },
+
+      // Calculate pw (Works like vw, but is a percentage of parent width)
+      pw: function(size){
+        var parentWidth = $parent.width();
+        return Math.ceil((parentWidth / 100) * size);
+      }
+    };
+    
+    variableFontSize = (getUnits() === 'vw' || 'pw') ? true : false; // Bool, indicates whether to recalculate font size inside resizer()
+    
     // Setup options
     var compressor = kompressor || 1,
         settings = $.extend({
@@ -20,14 +59,37 @@
           'maxFontSize' : Number.POSITIVE_INFINITY
         }, options);
 
+    var recalculate = function(options){
+
+      if(!options) { return; }
+
+      // Iterate over options
+      for(var fontSetting in options){
+        var size  = parseFloat(options[fontSetting]),
+            units = getUnits(options[fontSetting]);
+        
+        // If a function to calculate the given unit exists, call it and pass in the size argument
+        if(typeof calculateUnits[units] === 'function') {
+
+          // *Update the settings with calculated pixel values, options remain untouched
+          settings[fontSetting] = calculateUnits[units](size) + 'px';
+        }
+      }
+    };
+
     return this.each(function(){
 
       // Store the object
       var $this = $(this);
-
+      
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
+
+        // If variableFontSize is true, we have vw or pw units
+        // which require recalculating the font size.
+        if(variableFontSize) { recalculate(options); }
         $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      
       };
 
       // Call once to set.
@@ -35,7 +97,7 @@
 
       // Call on resize. Opera debounces their resize by default.
       $(window).on('resize.fittext orientationchange.fittext', resizer);
-
+      
     });
 
   };
